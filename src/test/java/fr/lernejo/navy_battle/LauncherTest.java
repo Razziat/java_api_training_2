87471dfad646a1ac50425d526cf4ab99ffe020c7
henri.thomas <henri.thomas@epita.fr>
2023-04-05 12:51:15 +0200

    add new test for coverage

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -1,60 +1,50 @@
 package fr.lernejo.navy_battle;
 
-import fr.lernejo.navy_battle.Launcher;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-public class LauncherTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+class LauncherTest {
+    @Test
+    public void testInvalidPortNumber() throws IOException {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(outContent));
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1023", "0", "-1", "65536", "9999999"})
-    public void testInvalidPortNumber(String portNumber) {
-        System.setOut(new PrintStream(output));
-        String[] args = {portNumber};
+        String[] args = {"-1"};
         try {
             Launcher.main(args);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals("Enter port number between 1024 and 65535:\n", output.toString());
+        assertEquals("Enter port number between 1024 and 65535:\n", outContent.toString());
+        outContent.reset();
+
+        String[] args2 = {"65536"};
+        try {
+            Launcher.main(args2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals("Enter port number between 1024 and 65535:\n", outContent.toString());
+        outContent.reset();
     }
 
     @Test
-    public void testNoArguments() {
-        System.setOut(new PrintStream(output));
+    public void testNoArguments() throws IOException {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(outContent));
+
         String[] args = {};
         try {
             Launcher.main(args);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals("No argument, program needs 1 or 2 arguments to be launched\n", output.toString());
+        assertEquals("No argument, program needs 1 or 2 arguments to be launched\n", outContent.toString());
     }
-
-    @Test
-    public void testServerOnly() throws IOException, InterruptedException {
-        String[] args = {"5000"};
-        Launcher.main(args);
-        // Here you can add your assertions to check if the server is started correctly.
-    }
-
-    @Test
-    public void testServerAndClient() throws IOException, InterruptedException {
-        String[] args = {"5000", "http://localhost:8080"};
-        Launcher.main(args);
-        // Here you can add your assertions to check if the server and client are started correctly.
-    }
-
+    
 }
